22beea1d907f0d785f33519d6cf49f87334a2b3d
Ash Laidlaw and David Quisenberry <david@>
2017-02-20 10:45:31 -0800

    Add second category

--- src/Task.php
+++ src/Task.php
@@ -2,10 +2,12 @@
     class Task
     {
         private $description;
+        private $id;
 
-        function __construct($description)
+        function __construct($description, $id = null)
         {
             $this->description = $description;
+            $this->id = $id;
         }
 
         function setDescription($new_description)
@@ -18,9 +20,15 @@
             return $this->description;
         }
 
+        function getId()
+        {
+            return $this->id;
+        }
+
         function save()
         {
             $GLOBALS['DB']->exec("INSERT INTO tasks (description) VALUES ('{$this->getDescription()}');");
+            $this->id = $GLOBALS['DB']->lastInsertId();
         }
 
         static function getAll()
@@ -30,7 +38,8 @@
 
             foreach ($returned_tasks as $task) {
                 $description = $task['description'];
-                $new_task = new Task($description);
+                $id = $task['id'];
+                $new_task = new Task($description, $id);
                 array_push($tasks, $new_task);
             }
 
@@ -40,6 +49,19 @@
         static function deleteAll()
         {
             $GLOBALS['DB']->exec("DELETE FROM tasks;");
+        }
+
+        static function find($search_id)
+        {
+          $found_task = null;
+          $tasks = Task::getAll();
+          foreach($tasks as $task) {
+            $task_id = $task->getId();
+            if ($task_id == $search_id) {
+              $found_task = $task;
+            }
+          }
+          return $found_task;
         }
     }
 ?>
