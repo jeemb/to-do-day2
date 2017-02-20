a6430629c81e5eba302bdcf2106487799ce292c6
Ash Laidlaw and David Quisenberry <ash@>
2017-02-20 11:54:14 -0800

    add ability to categorize tasks

--- src/Task.php
+++ src/Task.php
@@ -1,13 +1,15 @@
 <?php
-    class Task
+class Task
     {
         private $description;
+        private $category_id;
         private $id;
 
-        function __construct($description, $id = null)
+        function __construct($description, $id = null, $category_id)
         {
             $this->description = $description;
             $this->id = $id;
+            $this->category_id = $category_id;
         }
 
         function setDescription($new_description)
@@ -25,9 +27,14 @@
             return $this->id;
         }
 
+        function getCategoryId()
+        {
+            return $this->category_id;
+        }
+
         function save()
         {
-            $GLOBALS['DB']->exec("INSERT INTO tasks (description) VALUES ('{$this->getDescription()}');");
+            $GLOBALS['DB']->exec("INSERT INTO tasks (description, category_id) VALUES ('{$this->getDescription()}', {$this->getCategoryId()})");
             $this->id = $GLOBALS['DB']->lastInsertId();
         }
 
@@ -35,33 +42,32 @@
         {
             $returned_tasks = $GLOBALS['DB']->query("SELECT * FROM tasks;");
             $tasks = array();
-
-            foreach ($returned_tasks as $task) {
+            foreach($returned_tasks as $task) {
                 $description = $task['description'];
                 $id = $task['id'];
-                $new_task = new Task($description, $id);
+                $category_id = $task['category_id'];
+                $new_task = new Task($description, $id, $category_id);
                 array_push($tasks, $new_task);
             }
-
             return $tasks;
         }
 
         static function deleteAll()
         {
-            $GLOBALS['DB']->exec("DELETE FROM tasks;");
+          $GLOBALS['DB']->exec("DELETE FROM tasks;");
         }
 
         static function find($search_id)
         {
-          $found_task = null;
-          $tasks = Task::getAll();
-          foreach($tasks as $task) {
-            $task_id = $task->getId();
-            if ($task_id == $search_id) {
-              $found_task = $task;
+            $found_task = null;
+            $tasks = Task::getAll();
+            foreach($tasks as $task) {
+                $task_id = $task->getId();
+                if ($task_id == $search_id) {
+                  $found_task = $task;
+                }
             }
-          }
-          return $found_task;
+            return $found_task;
         }
     }
 ?>
